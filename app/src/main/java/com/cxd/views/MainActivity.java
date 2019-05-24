0b13c5782d5c9bd89b4909e85245c33738bfc102
cxd <qipq@qq.com>
2019-05-24 09:25:00 +0800

    修改了包名

--- app/src/main/java/com/cxd/views/MainActivity.java
+++ app/src/main/java/com/cxd/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.cxd.embeddedrecyclerview;
+package com.cxd.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- moudle/src/main/java/com/cxd/views/moudle/EmbeddedRecyclerView.java
+++ moudle/src/main/java/com/cxd/views/moudle/EmbeddedRecyclerView.java
@@ -1,4 +1,4 @@
-package com.cxd.embeddedrecyclerview.embeddedrecyclerview;
+package com.cxd.views.moudle;
 
 import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
