a8d37749211a5f2ad362580a5b901407b8c41cb7
cxd <qipq@qq.com>
2020-01-16 10:51:05 +0800

    change minVersion from 19 to 16

--- app/src/main/java/com/cxd/views/MainActivity.java
+++ app/src/main/java/com/cxd/views/MainActivity.java
@@ -3,8 +3,6 @@ package com.cxd.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.cxd.views.moudle.EmbeddedRecyclerView;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
